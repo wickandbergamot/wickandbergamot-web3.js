a46a55ce924ce6d813b9c644cedfcf8249365575
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-22 00:09:29 +0800

    Update ed25519.test.ts

--- packages/library-legacy/test/program-tests/ed25519.test.ts
+++ packages/library-legacy/test/program-tests/ed25519.test.ts
@@ -4,7 +4,7 @@ import {
   Connection,
   Keypair,
   sendAndConfirmTransaction,
-  LAMPORTS_PER_SAFE,
+  LAMPORTS_PER_WICKANDBERGAMOT,
   Transaction,
   Ed25519Program,
 } from '../../src';
@@ -21,7 +21,7 @@ if (process.env.TEST_LIVE) {
 
     before(async function () {
       await connection.confirmTransaction(
-        await connection.requestAirdrop(from.publicKey, 10 * LAMPORTS_PER_SAFE),
+        await connection.requestAirdrop(from.publicKey, 10 * LAMPORTS_PER_WICKANDBERGAMOT),
       );
     });
 
