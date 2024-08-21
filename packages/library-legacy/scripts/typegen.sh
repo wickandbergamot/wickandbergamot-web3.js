615c5eef76d14397db90346cc30b1ae18684be0e
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-21 23:51:04 +0800

    Update typegen.sh

--- packages/library-legacy/scripts/typegen.sh
+++ packages/library-legacy/scripts/typegen.sh
@@ -13,7 +13,7 @@ sed -i.bak '$s/export {.*};/}/' lib/index.d.ts
 sed -i.bak 's/declare/export/g' lib/index.d.ts
 
 # Prepend declare module line
-sed -i.bak '2s;^;declare module "@safecoin/web3.js" {\n;' lib/index.d.ts
+sed -i.bak '2s;^;declare module "@wickandbergamot/web3.js" {\n;' lib/index.d.ts
 
 # Remove backup file from `sed` above
 rm lib/index.d.ts.bak
