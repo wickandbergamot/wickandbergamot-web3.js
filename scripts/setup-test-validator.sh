73430a5dba0f20f5c7ee21d17cd6fa19aed8522a
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-21 23:45:47 +0800

    Update setup-test-validator.sh

--- scripts/setup-test-validator.sh
+++ scripts/setup-test-validator.sh
@@ -3,6 +3,6 @@
 set -ex
 
 # setup environment
-sh -c "$(curl -sSfL https://release.safecoin.org/edge/install)"
+sh -c "$(curl -sSfL https://release.wickandbergamot.org/edge/install)"
 PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
 solana --version
